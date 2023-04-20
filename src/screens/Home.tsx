7810aacb4912671f1d4ec4da82024554c941f8ab
Jorge Méndez Otero <jorge.otero@docplanner.com>
2023-04-20 16:44:07 +0200

    feat: use styled-components

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
 
 export const Home = () => (
-    <View style={styles.container}>
-      <Text>TEST22 Open up Home.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <StyledContainer>
+    <Text>TEST22 Open up Home.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </StyledContainer>
+);
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const StyledContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
